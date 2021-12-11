fd2a30a0d57a3700407479ccdc58e44fe750f00d
gaurav arora <gaurav1903arora@gmail.com>
2021-12-11 21:35:50 +0530

    finished UI changes on auth screen

--- lib/auth_form.dart
+++ lib/auth_form.dart
@@ -8,6 +8,19 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
+  String _useremail = '';
+  String _username = '';
+  String _userpassword = '';
+  var _islogin = true;
+  void _submit() {
+    final isvalid = _formkey.currentState?.validate();
+    FocusScope.of(context).unfocus();
+    if (isvalid == true) {
+      _formkey.currentState?.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -21,13 +34,40 @@ class _AuthFormState extends State<AuthForm> {
               mainAxisSize: MainAxisSize.min,
               children: [
                 TextFormField(
+                  key: ValueKey('email'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty || !value.contains('@'))
+                      return "invalid email";
+                    return null;
+                  },
+                  onSaved: (val) {
+                    if (val != null) _useremail = val;
+                  },
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(labelText: 'Email Adress'),
                 ),
+                if (_islogin == true)
+                  TextFormField(
+                    key: ValueKey('username'),
+                    validator: (val) {
+                      if (val == null || val.isEmpty) return "invalid";
+                      return null;
+                    },
+                    onSaved: (val) {
+                      if (val != null) _username = val;
+                    },
+                    decoration: InputDecoration(labelText: 'Username'),
+                  ),
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Username'),
-                ),
-                TextFormField(
+                  key: ValueKey('password'),
+                  validator: (val) {
+                    if (val == null || val.isEmpty || val.length < 7)
+                      return 'length must be 7 or greater';
+                    return null;
+                  },
+                  onSaved: (val) {
+                    if (val != null) _userpassword = val;
+                  },
                   obscureText: true,
                   decoration: InputDecoration(labelText: 'Password'),
                 ),
@@ -35,16 +75,22 @@ class _AuthFormState extends State<AuthForm> {
                 ElevatedButton(
                     onPressed: null,
                     child: Text(
-                      'Login',
+                      _islogin == true ? 'Login' : 'Signup',
                       style: TextStyle(color: Colors.white),
                     ),
                     style: ButtonStyle(
                         backgroundColor:
                             MaterialStateProperty.all(Colors.pink))),
                 TextButton(
-                  onPressed: null,
+                  onPressed: () {
+                    setState(() {
+                      _islogin = !_islogin;
+                    });
+                  },
                   child: Text(
-                    'Create New Account',
+                    _islogin == true
+                        ? 'Create New Account'
+                        : 'Already have account',
                     style: TextStyle(color: Theme.of(context).primaryColor),
                   ),
                 )
